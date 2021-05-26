3ba4f338729cc798be7831b90f8f0e06433df14d
River <junetheriver@gmail.com>
2021-05-27 00:38:19 +0800

    smallppm cloned

---
+++ code/include/hitrecord.hpp
@@ -0,0 +1,17 @@
+#ifndef HITRECORD_H
+#define HITRECORD_H
+
+#include <vecmath.h>
+
+struct HitRecord
+{
+    Vector3f pos;
+    Vector3f nrm;
+    Vector3f flux;
+    Vector3f f;
+    double r2;
+    unsigned int n;
+    int idx;
+};
+
+#endif // HITRECORD_H
